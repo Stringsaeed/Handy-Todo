d950f5fae5ea02878e5c0b91b338d503ff1127b1
Muhammed Saeed <stringsaeed@gmail.com>
2023-04-17 21:39:04 +0400

    Initial Commit

---
+++ Handy/HandyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HandyApp.swift
+//  Handy
+//
+//  Created by Muhammed Saeed on 17/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct HandyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
